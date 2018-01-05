5a0df909354961aa7049d6939970002ae7cbb89a
scisci <accounts.github@scisci.org>
2018-01-05 01:13:04 -0500

    add comment for djikstra attributor

---
+++ path_attributor.go
@@ -0,0 +1,7 @@
+package hambidgetree
+
+// Create a path finder filter
+// Chooses one or more paths through a tree and marks those nodes
+
+// Paths could be from node to node, edge to edge, etc.
+//
