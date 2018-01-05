a9d2e57f3513f11e5b82ab2e514bffa8be0bde28
scisci <accounts.github@scisci.org>
2018-01-05 01:02:55 -0500

    adds neighbor test

--- neighbors_test.go
+++ neighbors_test.go
@@ -19,19 +19,62 @@ func TestNeighbors2D(t *testing.T) {
 }
 
 func TestNeighbors3D(t *testing.T) {
-	for l := 3; l < 10; l++ {
-		tree := NewGridTree3D(l)
-		dimMap := NewNodeDimensionMap(tree, NewVector(0, 0, 0), 1.0)
-		leaves := tree.Leaves()
-		leaf := leaves[0]
+	tree := NewGridTree3D(3)
+	dimMap := NewNodeDimensionMap(tree, NewVector(0, 0, 0), 1.0)
+	leaves := tree.Leaves()
+	leaf := leaves[0]
 
+	neighbors, err := FindNeighbors(leaf, dimMap)
+	if err != nil {
+		t.Errorf("Error finding neighbors %v", err)
+	}
+
+	if len(neighbors) != 7 {
+		t.Errorf("3d Tree of %d leaves should always have 7 neighbors, got %d", len(leaves), len(neighbors))
+	}
+}
+
+func TestNeighbors3DMeasured(t *testing.T) {
+	tree := NewGridTree3D(6)
+	dimMap := NewNodeDimensionMap(tree, NewVector(0, 0, 0), 1.0)
+	leaves := tree.Leaves()
+
+	for _, leaf := range leaves {
 		neighbors, err := FindNeighbors(leaf, dimMap)
 		if err != nil {
 			t.Errorf("Error finding neighbors %v", err)
 		}
 
-		if len(neighbors) != 7 {
-			t.Errorf("3d Tree of %d leaves should always have 7 neighbors, got %d", l, len(neighbors))
+		dim, err := dimMap.Dimension(leaf)
+		if err != nil {
+			t.Errorf("Error finding dimension of leaf %v", err)
+		}
+
+		// Make sure that each leaf that is not in neighbors is actually not a
+		// neighbor.
+		for _, other := range leaves {
+			if other == leaf {
+				continue
+			}
+
+			isNeighbor := false
+			for _, n := range neighbors {
+				if n == other {
+					isNeighbor = true
+					break
+				}
+			}
+
+			otherDim, err := dimMap.Dimension(other)
+			if err != nil {
+				t.Errorf("Error getting dimension of neighbor %v", err)
+			}
+
+			isCalcNeighbor := dim.DistanceSquared(otherDim) < 0.0000001
+			if isCalcNeighbor != isNeighbor {
+				t.Errorf("Neighbor incorrect, expected %t, got %t", isNeighbor, isCalcNeighbor)
+			}
 		}
 	}
+
 }
